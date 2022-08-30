51f2f488650b155dbc52b592df9e8cfc83f37d4c
Simin-Kim <show3391@gmail.com>
2022-08-30 18:08:10 +0900

    init

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="react-scripts" />
+/*
+Typescript 타입 선언을 참조
+CRA로 만들어진 프로젝트에 특정하는 타입스크립트 타입 유형을 알려준다.
+여기에 있는 타입 유형은 bmp, gif, jpeg, jpg, png, webp, svg와 같은 리소스 파일 임포트를 지원
+.moudle.css, .module.scss 확장자를 가진 css 모듈 임포트 지원 추가
+*/
