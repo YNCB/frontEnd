ce567af8d918b92700d187bffe2846aa19010c3e
Simin-Kim <show3391@gmail.com>
2022-09-01 17:47:17 +0900

    Feat: React Router 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+
 import React from 'react';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Header from './components/header/Header';
+import MainPage from './pages/main/MainPage'
 
 function App() {
   return (
-    <div className="App">
-      
-    
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<MainPage/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
