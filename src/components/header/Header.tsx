d4bf9ed46c93f10b493319c43d906c64d469d68d
Simin-Kim <show3391@gmail.com>
2022-09-02 18:15:14 +0900

    Feat: Login 모달 컴포넌트 handle 기능 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,29 @@
 import {Link} from 'react-router-dom';
 import header from './Header.module.css'
+import ModalPortal from './../../ModalPortal'
+import LoginModal from './../login/LoginModal';
+import { useState } from 'react';
 
-function Header() {
+const Header = () => {
+
+    const [modal, setModal] = useState('')
+
+    const handleModalOff = () => {setModal('')}
+    const handleModalOn = () => {setModal('on')}
 
     return (
-        <header className={`${header.header}`}>
-            <Link to='/' className={`${header.logoImg}`}><img src={`${process.env.PUBLIC_URL}/assets/img/logo.png`} alt="logo" /></Link>
-            <div className={`${header.navContainer}`}>
-                <button>문제 등록</button>
-                <button>로그인</button>
-            </div>
-        </header>
+        <>
+            <header className={`${header.header}`}>
+                <nav className={`${header.nav}`}>
+                    <Link to='/' className={`${header.logoImg}`}><img src={`${process.env.PUBLIC_URL}/assets/img/logo.png`} alt="logo" /></Link>
+                    <div className={`${header.navContainer}`}>
+                        <button>문제 등록</button>
+                        <button onClick = { handleModalOn }>로그인</button>
+                    </div>
+                </nav>
+            </header>
+            {modal && <ModalPortal><LoginModal onClose={handleModalOff}/></ModalPortal>}
+        </>
     )
 }
 
