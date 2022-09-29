f9ead3e6655b3e8776b12ac82c210fdd2dfd30c2
Simin-Kim <show3391@gmail.com>
2022-09-29 16:42:26 +0900

    Chore: Route 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Header from './components/header/Header';
-import MainPage from './pages/main/MainPage'
+import Header from './components/Header/Header';
+import MyBoxPage from './pages/myBoxPage/MyBoxPage';
+import PostProblemPage from './pages/postProblemPage/PostProblemPage';
+import OthersPage from './pages/othersPage/OthersPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Header/>
       <Routes>
-        <Route path='/' element={<MainPage/>}></Route>
+        <Route path='/' element={<OthersPage/>} />
+        <Route path='/mybox' element={<MyBoxPage/>} />
+        <Route path='/post' element={<PostProblemPage/>} />
       </Routes>
     </BrowserRouter>
   );
