cfcb0fde5b8e8976f9735669c4379b8755a8cee8
Simin-Kim <show3391@gmail.com>
2022-09-29 16:39:26 +0900

    Feat: 페이지 프레임 추가

---
+++ src/pages/myBoxPage/MyBoxPage.tsx
@@ -0,0 +1,13 @@
+import Filter from '../../components/myBox/filter/Filter'
+import Main from '../../components/myBox/boxes/Boxes'
+
+const MyBoxPage = () => {
+    return (
+        <>
+            <Filter/>
+            <Main/>
+        </>
+    )
+}
+
+export default MyBoxPage
---
+++ src/pages/othersPage/OthersPage.tsx
@@ -0,0 +1,8 @@
+const OthersPage = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default OthersPage
