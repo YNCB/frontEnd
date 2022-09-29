ae79fd5bfcd521c44e4e9fa8736ca36a5b9cce87
Simin-Kim <show3391@gmail.com>
2022-09-29 16:40:23 +0900

    Refactor: css-module에서 styled-components로 리팩토링

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,8 @@
 import {Link} from 'react-router-dom';
-import header from './Header.module.css'
-import ModalPortal from './../../ModalPortal'
-import LoginModal from './../login/LoginModal';
+import ModalPortal from '../../ModalPortal'
+import LoginModal from '../LoginModal/LoginModal';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
 
@@ -13,18 +13,44 @@ const Header = () => {
 
     return (
         <>
-            <header className={`${header.header}`}>
-                <nav className={`${header.nav}`}>
-                    <Link to='/' className={`${header.logoImg}`}><img src={`${process.env.PUBLIC_URL}/assets/img/logo.png`} alt="logo" /></Link>
-                    <div className={`${header.navContainer}`}>
-                        <button>문제 등록</button>
+            <HeaderContainer>
+                <HeaderNav>
+                    <HeaderLogo to='/'><img src={`${process.env.PUBLIC_URL}/assets/img/logo.png`} alt="logo" /></HeaderLogo>
+                    <NavWrapper>
+                        <Link to = '/mybox'><button>내 박스</button></Link>
+                        <Link to = '/post'><button>문제 등록</button></Link>
                         <button onClick = { handleModalOn }>로그인</button>
-                    </div>
-                </nav>
-            </header>
+                    </NavWrapper>
+                </HeaderNav>
+            </HeaderContainer>
             {modal && <ModalPortal><LoginModal onClose={handleModalOff}/></ModalPortal>}
         </>
     )
 }
+
+const HeaderContainer = styled.header`
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+`
+
+const HeaderNav = styled.nav`
+    max-width: 1190px;
+    height: 85px;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const HeaderLogo = styled(Link)`
+`
+
+const NavWrapper = styled.div`
+    display: flex;
+    gap: 50px;
+    button {
+        font-family: 'SpoqaM';
+        font-size: 18px;
+    }
+`
 
 export default Header
