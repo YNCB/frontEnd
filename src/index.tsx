712981bffee276643e02601704fff296feae32ac
Simin-Kim <show3391@gmail.com>
2022-12-05 00:18:21 +0900

    Feat: redux-persist 기본 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import './font.css';
+import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import {store, persistor} from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import ScrollToTop from './components/common/ScrollToTop';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <App />
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
 );
---
+++ src/store/store.ts
@@ -0,0 +1,33 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore , FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import auth from "../reducers/auth";
+import modal from "../reducers/modal";
+
+const persistConfig = {
+    key: "root",
+    storage: storage,
+    // localstorage에 저장할 reducer 지정
+    whitelist: ["auth"],
+};
+
+// reducer를 뭉친것을 rootReducer 라고 정해준다.
+// reducer는 여러개 있을 수 있으므로 하나로 뭉쳐주는 작업이다.
+const rootReducer = combineReducers({ auth: auth.reducer, modal: modal.reducer });
+// persistReducer와 reducer를 뭉쳐놓은 덩어리를 같이 합쳐준다.
+// persistedReducer: Reducer에 이미 존재하는 액션(rootReducer) 외에 PERSIST, PURGE, FLUSH, PAUSE, REHYDRATE을 추가적으로 탐지해 특정 기능 수행
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    })
+});
+
+export const persistor = persistStore(store)
+export type RootState = ReturnType<typeof store.getState>
