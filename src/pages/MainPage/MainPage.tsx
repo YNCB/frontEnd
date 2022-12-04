ae159c10224fae155364e706a3c07302cb2fd298
Simin-Kim <show3391@gmail.com>
2022-12-05 00:20:40 +0900

    Feat: 페이지 atomic-design 적용

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,21 +1,21 @@
-import problemList from "../Datas/ProblemData"
+import problemList from '../Datas/ProblemData'
 import { myFilterList, othersFilterList } from "../Datas/FilterData"
 import Filter from "../../components/organisms/Filter"
-import Box from "../../components/organisms/Box"
-import BoxPageTitle from "../../components/atoms/BoxPageTitle"
+import Box from '../../components/organisms/Box'
+import BoxPageTitle from '../../components/atoms/BoxPageTitle'
 
-const MyBoxPage = () => {
+const MainPage = () => {
 
     // myFilterList, othersFilterList 분기 코드 필요 (지금 이 코드는 임시)
-    const filterList = [...myFilterList]
+    const filterList = [...othersFilterList]
 
     return (
         <>
-            <BoxPageTitle>Welcome to Your CODEBOX</BoxPageTitle>
+            <BoxPageTitle>Welcome to CODEBOX</BoxPageTitle>
             <Filter filterList={filterList}/>
             <Box problemList={problemList}/>
         </>
     )
 }
 
-export default MyBoxPage
+export default MainPage
