fb9a64e41fe9d9cb9c21629352f758eb82725b2d
Simin-Kim <show3391@gmail.com>
2022-12-05 00:13:13 +0900

    Feat: 카카오 로그인 구현을 위한 타입 설정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,3 +5,7 @@ CRA로 만들어진 프로젝트에 특정하는 타입스크립트 타입 유�
 여기에 있는 타입 유형은 bmp, gif, jpeg, jpg, png, webp, svg와 같은 리소스 파일 임포트를 지원
 .moudle.css, .module.scss 확장자를 가진 css 모듈 임포트 지원 추가
 */
+
+interface Window {
+    Kakao: any
+}
