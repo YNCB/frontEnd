c27f9b57fc0bd44a84e17159f0791df8b8923196
Simin-Kim <show3391@gmail.com>
2022-12-08 03:21:57 +0900

    Feat: Button 컴포넌트 atomic design pattern 적용

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,6 +1,57 @@
-const Button = () => {
-    
+import styled from "styled-components"
+
+interface ButtonProps {
+    children: React.ReactNode,
+    width?: string,
+    handler(): void,
+    color: string
+}
+
+const ButtonAtoms = ({children, width, handler, color}: ButtonProps) => {
+
     return (
-        <></>
+        <ButtonWrapper width={width ? width : '100%'}>
+            <Button onClick={() => handler()} color={color}>
+                {children}
+            </Button>
+        </ButtonWrapper>
     )
 }
+
+const ButtonWrapper = styled.div<{ width: string }>`
+    width: ${(props) => props.width};
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Button = styled.button<{ color: string }>`
+    width: 100%;
+    height: 100%;
+    font-size: 18px;
+    font-family: 'SpoqaM';
+    border: 1px solid #B4B4B4;
+    border-radius: 50px;
+    ${(props) => {
+            return {...theme[props.color]}
+        }
+    }
+`
+
+const theme: {[key: string]: {}} = {
+    black: {
+        backgroundColor: "#000",
+        color: "#fff"
+    },
+    grey: {
+        backgroundColor: "#D1D1D1",
+        color: "#333333"
+    },
+    white: {
+        backgroundColor: "#fff",
+        color: "#000"
+    }
+}
+
+export default ButtonAtoms
