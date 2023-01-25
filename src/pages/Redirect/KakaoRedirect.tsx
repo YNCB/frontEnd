959de54778a538c1f35aca5dd0322cd6818b4fb2
Simin-Kim <show3391@gmail.com>
2023-01-25 21:03:50 +0900

    카카오 로그인 API 연동

--- src/pages/Redirect/KakaoRedirect.tsx
+++ src/pages/Redirect/KakaoRedirect.tsx
@@ -1,65 +1,75 @@
 import React, { useEffect } from "react";
-import { getKakaoAuthCode } from "../../api/user/user";
-import { useDispatch } from "react-redux";
+import { getKakaoAuthCode } from "../../apis/api/user";
+import { useDispatch, useSelector } from "react-redux";
 import { changeModal } from "../../store/slices/modalSlice";
-import axios from "axios";
+import { RootState } from "../../store/config";
+import { setUserInfo } from "../../store/slices/userSlice";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { Axios, AxiosResponse } from "axios";
 
 const KakaoRedirect = () => {
-
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    const user = useSelector((state:RootState) => state.user);
     
     useEffect(() => {
         const params = new URL(document.location.toString()).searchParams;
         const authCode = String(params.get("code"));
-        const config = {
-            headers : {
+        const body = {
+            params : {
                 "code" : authCode
             }
         }
         
         async function getKakaoAuthCodeApi () {
             try {
-                const response = await getKakaoAuthCode(config);
+                const response = await getKakaoAuthCode(body);
                 const status = response.status;
-                const data = response.data;
+                const data = response.data.data;
+                console.log(response);
 
                 if (status === 200) {
-                    alert('로그인 성공')
+                    const userInfo = {
+                        user_Id: data.user_Id,
+                        nickname: data.nickname,
+                        accessToken: data.accessToken,
+                        refreshToken: data.refreshToken,
+                        my_session: data.my_session,
+                        expireTime: data.expireTime
+                    }
+                    dispatch(setUserInfo(userInfo));
+
+                    Swal.fire({
+                        title: '로그인 성공',
+                        text: 'CODEBOX에 오신 것을 환영합니다!',
+                        icon: 'success'
+                    })
+                    .then(() => {
+                        dispatch(changeModal(0));
+                        navigate('/');
+                    })
                 }
                 else if (status === 201) {
                     const payload = {
                         page: 4,
                         email: data.email,
-                        nickName: data.nickname,
+                        nickname: data.nickname,
                         password: data.password,
                         socialType: data.social_type
                     }
-                    dispatch(changeModal(payload))
-                }
-                else {
-                    alert(`${status} 에러 : 관리자에게 문의하세요.`)
+                    dispatch(changeModal(payload));
                 }
             }
-            catch (err) {
-                alert(`catch ${err} 에러 : 관리자에게 문의하세요.`)
+            catch (err: any) {
+                const status = err.response.status;
+
+                alert(`${status} 에러 : 관리자에게 문의하세요.`);
             }
         }
         getKakaoAuthCodeApi();
-
-        // axios
-        //   .get(`${process.env.REACT_APP_BACKEND_BASE_URL}/codebox/login/token/kakao`,
-        //     {
-        //       headers: {
-        //         "code" : authCode
-        //       }
-        //     }
-        //   )
-        //   .then((res) => {
-        //     console.log(res)
-        //   })
-        //   .catch((err) => {
-        //     console.log(err)
-        //   })
+        
     }, [])
 
 
