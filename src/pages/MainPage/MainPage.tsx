8df0e129ca2078ca36078723c5ccf38e2851fdb8
Simin-Kim <show3391@gmail.com>
2023-01-25 21:04:54 +0900

    Feat: 로그아웃 API 연동

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,11 +3,16 @@ import { myFilterList, othersFilterList } from "../Datas/FilterData"
 import Filter from "../../components/organisms/Filter"
 import Box from '../../components/organisms/Box'
 import BoxPageTitle from '../../components/atoms/BoxPageTitle'
+import { useEffect } from 'react'
 
 const MainPage = () => {
 
     // myFilterList, othersFilterList 분기 코드 필요 (지금 이 코드는 임시)
     const filterList = [...othersFilterList]
+
+    useEffect(()=>{
+        
+    },[])
 
     return (
         <>
