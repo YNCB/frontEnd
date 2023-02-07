f970169452e6531ab9e7a2e1ea3f253420a0cb30
Simin-Kim <show3391@gmail.com>
2023-02-08 00:30:59 +0900

    Fix: 라우팅 path 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/organisms/Header';
-import MyBoxPage from './pages/myBoxPage/MyBoxPage';
-import PostProblemPage from './pages/postProblemPage/PostProblemPage';
-import MainPage from './pages/MainPage/MainPage';
+import MainBox from './pages/boxPage/MainBox';
+import UserBox from './pages/boxPage/UserBox';
+import PostProblemPage from './pages/postProblem/PostProblemPage';
 import KakaoRedirect from './pages/Redirect/KakaoRedirect';
 import ScrollToTop from './components/common/ScrollToTop';
 
@@ -13,8 +13,8 @@ function App() {
 			<ScrollToTop/>
 			<Header/>
 			<Routes>
-			<Route path='/' element={<MainPage/>} />
-			<Route path='/mybox' element={<MyBoxPage/>} />
+			<Route path='/' element={<MainBox/>} />
+			<Route path='/userbox' element={<UserBox/>} />
 			<Route path='/post' element={<PostProblemPage/>} />
 			<Route path='/login/oauth2' element={<KakaoRedirect/>} />
 			</Routes>
