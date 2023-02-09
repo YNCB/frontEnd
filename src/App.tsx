7f6a47c4e0fa9e39c16ba0cdee2f0fa4ff4e05f3
Simin-Kim <show3391@gmail.com>
2023-02-09 21:35:12 +0900

    Fix: 라우팅 path 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/organisms/Header';
-import MainBox from './pages/boxPage/MainBox';
-import UserBox from './pages/boxPage/UserBox';
-import PostProblemPage from './pages/postProblem/PostProblemPage';
+import MainBox from './pages/box/MainBox';
+import UserBox from './pages/box/UserBox';
+import PostBox from './pages/problem/postBox';
 import KakaoRedirect from './pages/Redirect/KakaoRedirect';
 import ScrollToTop from './components/common/ScrollToTop';
+import Follower from './pages/info/follower';
+import MyInfoSetting from './pages/info/myInfoSetting';
+import DetailBox from './pages/problem/detailBox';
 
 function App() {
     return (
@@ -15,8 +18,11 @@ function App() {
 			<Routes>
 			<Route path='/' element={<MainBox/>} />
 			<Route path='/userbox' element={<UserBox/>} />
-			<Route path='/post' element={<PostProblemPage/>} />
+			<Route path='/postBox' element={<PostBox/>} />
+			<Route path='/detailBox' element={<DetailBox/>} />
 			<Route path='/login/oauth2' element={<KakaoRedirect/>} />
+			<Route path='/follower' element={<Follower/>} />
+			<Route path='/setting' element={<MyInfoSetting/>} />
 			</Routes>
 		</BrowserRouter>
     );
