f7682aededd0567f962102204543be9524feb454
Simin-Kim <show3391@gmail.com>
2023-02-11 00:51:21 +0900

    Feat: 문제 상세 페이지 구현

--- src/pages/problem/detailBox.tsx
+++ src/pages/problem/detailBox.tsx
@@ -1,11 +1,204 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import * as S from './detailBoxStyle'
+import { deleteMyBox, getDetailBox, postLike } from "../../apis/api/post";
+import { RootState } from "../../store/config";
+import { Viewer } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import { BoxDetailInterface, ReplyInterface } from "../../interfaces/boxDetailInterface";
+import moment from "moment";
+import ButtonAtom from "../../components/atoms/Button";
+import { postReply } from "../../apis/api/reply";
+import Swal from "sweetalert2";
+import Reply from "../../components/organisms/Reply";
 
 const DetailBox = () => {
+    const user = useSelector((state: RootState) => state.user);
+    const location = useLocation();
+    const {nickname, postId} = location.state as {nickname: string, postId: string};
+    const [isMyBox,] = useState(user.nickname === nickname);
+    const [postInfo, setPostInfo] = useState<BoxDetailInterface>({
+        title: "",
+        nickname: "",
+        redate: "",
+        likeExisting: false,
+        likeNum: null,
+        countView: null,
+        tags: [],
+        type: "ALONE",
+        language: "",
+        level: null,
+        context: "",
+        replyNum: null,
+        replies: [
+
+        ],
+        post_id: null,
+    });
+    const {title, redate, likeNum, likeExisting, countView, tags, type, language, level, context, replyNum, replies} = postInfo;
+    const [replyInputs, setReplyInputs] = useState('')
+    
+    useEffect(() => {
+        requestDetailBox();
+    }, [])
+
+    const requestDetailBox = useCallback( async () => {
+        const token = user.accessToken ? true : false;
+        try {
+            const response = await getDetailBox({nickname, postId}, token);
+            const {status, data} = response.data;
+            
+            if (status === '200') {
+                setPostInfo(data)
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }, [])
+
+    const requestDeleteBox = async () => {
+        try{
+            const response = await deleteMyBox({nickname, postId})
+            const {status, data} = response.data;
+
+            console.log(response);
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+    const requestLike = async () => {
+        try {
+            const response = await postLike({nickname: user.nickname, postId});
+            const {status, data} = response.data;
+            
+            if (status === '200') {
+                setPostInfo({
+                    ...postInfo,
+                    likeExisting: data.existing,
+                    likeNum: data.num
+                })
+            } 
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
+    const requestAddReply = async () => {
+        const body = {
+            nickname: user.nickname,
+            postId,
+            replySaveForm: {
+                content: replyInputs,
+                parent_id: null,
+                title: 'title'
+            }
+        }
+
+        try {
+            const response = await postReply(body);
+            const {status} = response.data;
+
+            if (status === '200') {
+                requestDetailBox();
+                setReplyInputs('');
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 
     return (
-        <>
-        </>
+        <S.DetailBoxContainer>
+            <S.DetailBoxSection>
+                <S.DetailBoxHeaderContainer>
+                    <S.DetailBoxTItle>{title}</S.DetailBoxTItle>
+                    <S.DetailBoxHeaderInfoContainer>
+                        <S.DetailBoxHeaderInfo>
+                            <span>{nickname} | </span>
+                            <span>{redate} | </span>
+                            <span>
+                                <img src="/assets/img/heart.svg" alt={'좋아요'}/>
+                                {likeNum}
+                            </span>
+                        </S.DetailBoxHeaderInfo>
+                        <S.DetailBoxSetting>
+                            {!!isMyBox && (
+                                <>
+                                    <button><span>수정</span></button>
+                                    <button onClick={requestDeleteBox}><span>삭제</span></button>
+                                </>
+                            )}
+                        </S.DetailBoxSetting>
+                    </S.DetailBoxHeaderInfoContainer>
+                </S.DetailBoxHeaderContainer>
+                <S.DetailBoxInfoContainer>
+                    <S.DetailBoxInfoListContainer>
+                        <S.DetailBoxInfoList>
+                            <p>태그</p>
+                            <S.DetailBoxTags>
+                                {tags.map((item,idx) => (
+                                    <p key={idx}>#{item}</p>
+                                ))}
+                            </S.DetailBoxTags>
+                        </S.DetailBoxInfoList>
+                        <S.DetailBoxInfoList>
+                            <p>문제 분류</p>
+                            <p>{type === 'ALONE' ? '혼자 푼 문제' : '보고 푼 문제'}</p>
+                        </S.DetailBoxInfoList>
+                        <S.DetailBoxInfoList>
+                            <p>사용 언어</p>
+                            <p>{language}</p>
+                        </S.DetailBoxInfoList>
+                        <S.DetailBoxInfoList>
+                            <p>난이도</p>
+                            <p>{level}</p>
+                        </S.DetailBoxInfoList>
+                    </S.DetailBoxInfoListContainer>
+                </S.DetailBoxInfoContainer>
+                <S.DetailBoxContentContainer>
+                    <h3>풀이</h3>
+                    {context && (
+                    <Viewer initialValue={context && context}/>
+                    )}
+                </S.DetailBoxContentContainer>
+                <S.DetailBoxLikesRepliesContainer>
+                    <S.DetailBoxLikesContainer>
+                        <S.DetailBoxLikes onClick={requestLike}>
+                        {
+                            likeExisting ?
+                            <svg width="38" height="33" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.0534 3.24444L10.411 3.5867L10.7568 3.23264C10.8899 3.09647 11.0177 2.96379 11.1419 2.83499C12.3287 1.60377 13.1748 0.726081 14.8984 0.530692C16.6971 0.329809 18.4321 1.1338 19.4859 2.438C20.53 3.73005 20.8924 5.49188 19.9925 7.25341L19.9924 7.25352C19.462 8.29243 18.3541 9.57013 17.0721 10.8711L17.072 10.8712C16.1897 11.767 15.2619 12.6382 14.4174 13.4313C13.9151 13.903 13.4422 14.3471 13.0258 14.7523C13.0258 14.7523 13.0258 14.7523 13.0258 14.7523L10.3956 17.3111L8.28668 15.3204C7.79962 14.8604 7.27445 14.3908 6.73368 13.9073C5.58809 12.883 4.37243 11.796 3.3005 10.6044C1.73044 8.85895 0.581875 7.00237 0.503069 5.00338C0.408 2.29452 2.57027 0.519937 5.16667 0.552219C6.33448 0.567774 7.13988 0.856367 7.84696 1.31273C8.46878 1.71406 9.01526 2.24225 9.67854 2.88332C9.79924 2.99998 9.92382 3.12039 10.0534 3.24444Z" fill="#ED1B24" stroke="black"/>
+                            </svg>
+                            :
+                            <svg width="38" height="33" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.0534 3.24444L10.411 3.5867L10.7568 3.23264C10.8899 3.09647 11.0177 2.96379 11.1419 2.83499C12.3287 1.60377 13.1748 0.726081 14.8984 0.530692C16.6971 0.329809 18.4321 1.1338 19.4859 2.438C20.53 3.73005 20.8924 5.49188 19.9925 7.25341L19.9924 7.25352C19.462 8.29243 18.3541 9.57013 17.0721 10.8711L17.072 10.8712C16.1897 11.767 15.2619 12.6382 14.4174 13.4313C13.9151 13.903 13.4422 14.3471 13.0258 14.7523C13.0258 14.7523 13.0258 14.7523 13.0258 14.7523L10.3956 17.3111L8.28668 15.3204C7.79962 14.8604 7.27445 14.3908 6.73368 13.9073C5.58809 12.883 4.37243 11.796 3.3005 10.6044C1.73044 8.85895 0.581875 7.00237 0.503069 5.00338C0.408 2.29452 2.57027 0.519937 5.16667 0.552219C6.33448 0.567774 7.13988 0.856367 7.84696 1.31273C8.46878 1.71406 9.01526 2.24225 9.67854 2.88332C9.79924 2.99998 9.92382 3.12039 10.0534 3.24444Z" fill="white" stroke="black"/>
+                            </svg>
+                        }
+                        </S.DetailBoxLikes>
+                    </S.DetailBoxLikesContainer>
+                    <S.DetailBoxRepliesContainer>
+                        <h5>{replyNum}개의 댓글</h5>
+                        <S.DetailBoxReplyAddContainer>
+                            <textarea placeholder="댓글을 작성하세요" onChange={(e) => setReplyInputs(e.target.value)} value={replyInputs}>
+
+                            </textarea>
+                            <ButtonAtom color='black' handler={()=>{requestAddReply()}} width='130px'>댓글 작성</ButtonAtom>
+                        </S.DetailBoxReplyAddContainer>
+                        {
+                        replies && (
+                        <Reply replies={replies} postId={postId} requestDetailBox={requestDetailBox}/>
+                        )
+                        }
+                    </S.DetailBoxRepliesContainer>
+                </S.DetailBoxLikesRepliesContainer>
+            </S.DetailBoxSection>
+        </S.DetailBoxContainer>
     )
 }
 
--- src/pages/problem/detailBoxStyle.tsx
+++ src/pages/problem/detailBoxStyle.tsx
@@ -1,2 +1,137 @@
 import styled from "styled-components";
 
+export const DetailBoxContainer = styled.div`
+    width: 1190px;
+    margin: 0 auto;
+`
+
+export const DetailBoxSection = styled.section`
+    padding: 135px;
+`
+
+export const DetailBoxHeaderContainer = styled.div`
+    padding-bottom: 30px;
+    margin-bottom: 60px;
+    border-bottom: 3px solid #F2F2F2;
+`
+
+export const DetailBoxTItle = styled.h2`
+    font-size: 32px;
+`
+
+export const DetailBoxHeaderInfoContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const DetailBoxHeaderInfo = styled.div`
+    display: flex;
+    gap: 10px;
+
+    & > span: nth-of-type(3) {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+
+        & > img {
+            width: 15px;
+        }
+    }
+`
+
+export const DetailBoxSetting = styled.div`
+    display: flex;
+    gap: 10px;
+    & > button {
+        font-size: 16px;
+        font-family: 'SpoqaR';
+        color: #717171;
+    }
+`
+
+export const DetailBoxInfoContainer = styled.div`
+    margin-bottom: 90px;
+`
+
+export const DetailBoxInfoListContainer = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+export const DetailBoxInfoList = styled.li `
+    display: flex;
+    font-size: 20px;
+
+    & > p:first-of-type {
+        color: #717171;
+        margin-right: 32px;
+        width: 100px;
+    }
+`
+
+export const DetailBoxTags = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+`
+
+export const DetailBoxContentContainer = styled.div`
+    margin-bottom: 250px;
+
+    & > h3 {
+        font-size: 20px;
+        border-bottom: 3px solid #F2F2F2;
+        padding-bottom: 20px;
+    }
+`
+
+export const DetailBoxLikesRepliesContainer = styled.div`
+
+`
+
+export const DetailBoxLikesContainer = styled.div`
+    margin-bottom: 60px;
+`
+
+export const DetailBoxLikes = styled.div`
+    width: 70px;
+    height: 70px;
+    border: 1px solid #000000;
+    border-radius: 10px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+`
+
+export const DetailBoxRepliesContainer = styled.div`
+    & > h5 {
+        font-size: 18px;
+    }
+`
+
+export const DetailBoxReplyAddContainer = styled.div`
+    & > textarea {
+        width: 100%;
+        border: 1px solid #b4b4b4;
+        border-radius: 5px;
+        resize: none;
+        height: 100px;
+        margin-top: 10px;
+        padding: 10px;
+        font-size: 16px;
+        font-family: 'SpoqaR';
+    }
+    & > textarea:focus {
+        outline: 0;
+    }
+
+    & > div {    
+        margin-left: auto;
+        margin-top: 15px;
+    }
+`
