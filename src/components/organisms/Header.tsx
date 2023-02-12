f0a6005e87daa3bd539cef537130ef7d59ea937d
Simin-Kim <show3391@gmail.com>
2023-02-12 21:56:02 +0900

    Feat: 문제 등록 페이지 state 전달

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -72,7 +72,7 @@ const Header = () => {
                     {user.authenticated ? (
                     <NavWrapper>
                         <Link to={'/userbox'} state={{ nickname: user.nickname || '' }}><button>내 박스</button></Link>
-                        <Link to='/postBox'><button>문제 등록</button></Link>
+                        <Link to='/postBox' state={{ isEdit: false, postId: null }}><button>문제 등록</button></Link>
                         <button onClick = { handleDropDown }>
                             <span>{user.nickname}</span>
                             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M7 10l5 5 5-5z"></path></svg>
